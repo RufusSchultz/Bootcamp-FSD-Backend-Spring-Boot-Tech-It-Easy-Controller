4d61273115fbe8e485f5afb7f871b3c5393dfd7d
RufusSchultz <rufusschultz@gmail.com>
2024-05-24 09:43:03 +0200

    setup

---
+++ src/test/java/com/techiteasy/controller/BackendSpringBootTechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiteasy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootTechItEasyControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
